e6db1551a757219df6da3ca749b30d11c07ba2d9
Sheremetova <evgeniya.sheremetova@kaspersky.com>
2019-07-17 10:13:30 +0300

    Init test file

---
+++ test_news.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+import unittest
+
+from appium import webdriver
+
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+
+class SimpleAndroidTests(unittest.TestCase):
+    def set_up(self):
+        desired_capabilities = {
+            'appPackage': 'my.deler.newstestapplication',
+            'appActivity': 'my.deler.newstestapplication.screens.MainActivity',
+            'platformName': 'Android',
+            'platformVersion': '7.0',
+            'deviceName': 'Android Emulator',
+            'app': PATH('apk/news.apk'),
+            'noReset': True
+            }
+
+
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities)
